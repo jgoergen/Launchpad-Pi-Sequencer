9cbe2f656af02138ee2732f1db6c5b8587777259
Jeff Goergen <danthestickman@yahoo.com>
2018-02-09 18:52:30 -0500

    launchpad 1 pad updates

--- defaults.js
+++ defaults.js
@@ -30,17 +30,17 @@ module.exports = {
     saveDisplay: [false,true,true,true,false,true,true,false,true,false,false,false,true,false,false,true,false,true,true,true,true,true,true,true,true,true,true,true,true,false,false,true,true,false,false,true,true,true,true,true,true,false,false,true,true,false,false,true,true,false,false,true,true,true,true,false,false,true,true,false,true,true,true,true],
 
     pads: [
-        81, 82, 83, 84, 85, 86, 87, 88,
-        71, 72, 73, 74, 75, 76, 77, 78,
-        61, 62, 63, 64, 65, 66, 67, 68,
-        51, 52, 53, 54, 55, 56, 57, 58,
-        41, 42, 43, 44, 45, 46, 47, 48,
-        31, 32, 33, 34, 35, 36, 37, 38,
-        21, 22, 23, 24, 25, 26, 27, 28,
-        11, 12, 13, 14, 15, 16, 17, 18
+        0, 1, 2, 3, 4, 5, 6, 7,
+        16, 17, 18, 19, 20, 21, 22, 23,
+        32, 33, 34, 35, 36, 37, 38, 39,
+        48, 49, 50, 51, 52, 53, 54, 55,
+        64, 65, 66, 67, 68, 69, 70, 71,
+        80, 81, 82, 83, 84, 85, 86, 87,
+        96, 97, 98, 99, 100, 101, 102, 103,
+        112, 113, 114, 115, 116, 117, 118, 119
     ],
       
-    trackPads: [89, 79, 69, 59, 49, 39, 29, 19],
+    trackPads: [8, 24, 40, 56, 72, 88, 104, 120],
     controlPads: [104, 105, 106, 107, 108, 109, 110, 111],
 
     emptyTrackData: [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false],
